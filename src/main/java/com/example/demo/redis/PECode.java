a24f001f26a1688a2117a6c70a75c463f5fd2c89
lingang.zhaolg <lingang.zhao@ximalaya.com>
2019-09-25 20:17:26 +0800

    位数转换代码优化

--- src/main/java/com/example/demo/redis/PECode.java
+++ src/main/java/com/example/demo/redis/PECode.java
@@ -6,44 +6,54 @@ package com.example.demo.redis;
  * @date 2019/9/25 2:29 下午
  */
 public class PECode {
-    /**
-     * 打乱改字符数组的组合顺序，可以得到不同的转换结果
-     */
-    private static final char[] digits = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '8', '5', '2', '7', '3', '6', '4', '0', '9', '1', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '+', '-'};
+
+    private static final int DIGIT = 1 << 6;
+
+    private static final int ZERO = 0;
+
+    private static final char[] digits = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f',
+            'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '8', '5', '2', '7', '3', '6', '4', '0',
+            '9', '1', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+            'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '+', '-'};
 
     /**
-     * @param number double类型的10进制数,该数必须大于0
+     * @param number long 类型的10进制数,该数必须大于0
      * @return string类型的64进制数
      */
     public static String encode(long number) {
-        char[] buf = new char[64];
-        int charPos = 64;
+        if (number <= 0) {
+            throw new RuntimeException("PECode.encode number Must be greater than 0");
+        }
+        char[] buf = new char[DIGIT];
+        int charPos = DIGIT;
         int radix = 1 << 6;
         long mask = radix - 1;
         do {
             buf[--charPos] = digits[(int) (number & mask)];
             number >>>= 6;
-        } while (number != 0);
-        return new String(buf, charPos, (64 - charPos));
+        } while (number != ZERO);
+        return new String(buf, charPos, (DIGIT - charPos));
     }
 
     /**
      * 支持范围是A-Z,a-z,0-9,+,-
      *
-     * @param decompStr 64进制的数字
+     * @param decoStr 64进制的数字
      * @return 10进制的数字
      */
-    public static long decode(String decompStr) {
-        long result = 0;
-        for (int i = decompStr.length() - 1; i >= 0; i--) {
-            for (int j = 0; j < digits.length; j++) {
-                if (decompStr.charAt(i) == digits[j]) {
-                    result += ((long) j) << 6 * (decompStr.length() - 1 - i);
+    public static long decode(String decoStr) {
+        if (decoStr == null || decoStr.length() == 0) {
+            throw new RuntimeException("PECode.decode decoStr not null ");
+        }
+        long result = ZERO;
+        for (int i = decoStr.length() - 1; i >= 0; i--) {
+            for (int j = ZERO; j < digits.length; j++) {
+                if (decoStr.charAt(i) == digits[j]) {
+                    result += ((long) j) << 6 * (decoStr.length() - 1 - i);
                 }
             }
         }
         return result;
     }
-
 
 }
