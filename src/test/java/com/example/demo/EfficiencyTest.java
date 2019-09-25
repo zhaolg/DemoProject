27ba76e4812470b0a8a758c1d540ba1bae044644
lingang.zhaolg <lingang.zhao@ximalaya.com>
2019-09-25 17:48:51 +0800

    64位压缩算法解压耗时测试

--- src/test/java/com/example/demo/EfficiencyTest.java
+++ src/test/java/com/example/demo/EfficiencyTest.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.redis.PECode;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /**
  * @author lingang.zhao
  * @version 1.0
@@ -9,5 +14,23 @@ public class EfficiencyTest {
 
     public static void main(String[] args) {
 
+        String keyPrefix = "01:201155:";
+        long business = 10054595993888L;
+        long startTimes = System.currentTimeMillis();
+        List<String> rtn = Lists.newArrayList();
+        for (int i = 0; i < 1000001; i++) {
+            long def = business + i;
+            rtn.add(PECode.encode(def));
+        }
+        long cost = System.currentTimeMillis() - startTimes;
+        System.out.println("encode cost:" + cost);
+
+        long startTimes2 = System.currentTimeMillis();
+        rtn.forEach(s -> {
+            long decode = PECode.decode(s);
+        });
+        long decodeCost = System.currentTimeMillis() - startTimes2;
+        System.out.println("decode cost:" + decodeCost);
     }
+
 }
